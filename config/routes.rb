8f1bcbab28ae30eef6daef2d4ea7b76b76417508
SpeakingP0tato <96wlgh@gmail.com>
2025-02-25 20:00:52 +0900

    Home#topのテストをリファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get "home/top"
-  
-  # 루트 경로 추가 (루트 URL 접근 시 home#top을 실행)
-  root "home#top"
-
-  # 기본적으로 제공되는 Rails health check (유지)
-  get "up" => "rails/health#show", as: :rails_health_check
+  root 'home#top'
 end
