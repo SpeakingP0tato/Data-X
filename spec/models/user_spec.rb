9ba7b4ecf7fc4a9e3ee10a15173fe4fc2c5d0c65
SpeakingP0tato <96wlgh@gmail.com>
2025-02-26 12:57:00 +0900

    User.nickname の文字数に関するバリデーションを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,32 +2,29 @@ require 'rails_helper'
 
 describe User do
   let(:nickname) { 'テスト太郎' }
-  let(:email) { 'test@example.com' }
   let(:password) { '12345678' }
-  let(:user) { User.new(nickname: nickname, email: email, password: password, password_confirmation: password) }
 
   describe '.first' do
     before do
-      create(:user, nickname: nickname, email: email)
+      @user = create(:user, nickname: nickname, password: password, password_confirmation: password)
     end
 
     subject { described_class.first }
 
     it '事前に作成した通りのUserを返す' do
       expect(subject.nickname).to eq('テスト太郎')
-      expect(subject.email).to eq('test@example.com')
+      expect(subject.email).to eq(@user.email)
     end
   end
 
   describe 'validation' do
-
     describe 'nickname属性' do
-
       describe '文字数制限の検証' do
         context 'nicknameが20文字以下の場合' do
           let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' } # 20文字
 
           it 'User オブジェクトは有効である' do
+            user = build(:user, nickname: nickname, password: password, password_confirmation: password)
             expect(user.valid?).to be(true)
           end
         end
@@ -36,8 +33,7 @@ describe User do
           let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' } # 21文字
 
           it 'User オブジェクトは無効である' do
-            user.valid?
-
+            user = build(:user, nickname: nickname, password: password, password_confirmation: password)
             expect(user.valid?).to be(false)
             expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
           end
